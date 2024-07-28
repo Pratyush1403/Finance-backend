73b5eea218f917f9a22621f8708d5477a2554bbe
Pratyush Sharma <pratyushsharma1404@gmail.com>
2024-07-28 21:29:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,16 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cors());
+const corsOptions = {
+  origin: 'https://financemanagementdashboard-4xqwn373x-pratyush-sharmas-projects.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
+
+
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
@@ -32,12 +41,9 @@ app.use("/transaction", transactionRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URL)
   .then(async () => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+    console.log(`Server connected at Port: ${PORT}`);
 
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
     // await mongoose.connection.db.dropDatabase();
@@ -45,4 +51,9 @@ mongoose
     // Product.insertMany(products);
     // Transaction.insertMany(transactions);
   })
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process with an error code
+  });
+
+export default app;
